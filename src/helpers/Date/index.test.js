b6b75573c0577a1348ff3014eb86a96bd3fa5a9a
Kristina Kuster <kriskus@live.fr>
2023-10-02 11:23:26 +0200

    Functional nav menu + 3 unit tests done

--- src/helpers/Date/index.test.js
+++ src/helpers/Date/index.test.js
@@ -1,14 +1,18 @@
-/**
- * 
- */
+import { getMonth, MONTHS } from "./index"
 
 describe("Date helper", () => {
     describe("When getMonth is called", () => {
         it("the function return janvier for 2022-01-01 as date", () => {
             // to implement
+           const mockDate = new Date("2022-01-01");
+           const month = getMonth(mockDate, MONTHS)
+           expect(month).toBe('janvier')
         });
         it("the function return juillet for 2022-07-08 as date", () => {
             // to implement
+            const mockDate = new Date("2022-07-08");
+            const month = getMonth(mockDate, MONTHS)
+            expect(month).toBe('juillet')
         });
     });
 })
